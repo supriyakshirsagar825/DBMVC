d3c6cce0b380fe6989e28d7152148653909a16b9
Supriya Kshirsagar <supriyakshirsagar9@gmail.com>
2021-03-17 14:52:23 -0400

    Added Class Lib For Model Layer

---
+++ ModelLayer/EmployeeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Nullable<int> Salary { get; set; }
+        public Nullable<int> AddressId { get; set; }
+        public string Gender { get; set; }
+
+
+    }
+}
