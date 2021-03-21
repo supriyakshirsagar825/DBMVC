725577907cf95d45b6e6e0112e3a43aa8da66790
Supriya Kshirsagar <supriyakshirsagar9@gmail.com>
2021-03-21 17:54:24 -0400

    MVC and jquery get ,post and ajax call from View

--- DBMVC/Controllers/TestController.cs
+++ DBMVC/Controllers/TestController.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using CLModelLayer;
+using Newtonsoft.Json;
 namespace DBMVC.Controllers
 {
     public class TestController : Controller
     {
-        // GET: Test
         public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Test
+        public JsonResult GetEmployeeWithJson()
+        {
+            EmployeeModel e = new EmployeeModel()
+            {
+                FirstName = "sup",
+                LastName = "ska",
+                Gender = "female",
+                Salary = 10000,
+                Address = new AddressModel()
+                {
+                    AddressLine1 = "1 line",
+                    AddressLine2 = "2 line",
+                    City = "pune",
+                    Country = "india",
+                    State = "mahahrashtra"
+                }
+            };
+            var json = JsonConvert.SerializeObject(e);
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        public void PostGetEmp(EmployeeModel m)
+        {
+
+        }
+        public ActionResult Index3()
         {
             return View();
         }
