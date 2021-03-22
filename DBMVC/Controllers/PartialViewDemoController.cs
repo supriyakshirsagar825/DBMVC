c29c005f082de7f8679fdd89da0751971ab46db2
Supriya Kshirsagar <supriyakshirsagar9@gmail.com>
2021-03-22 16:44:50 -0400

    @{ html.renderpartial} and @html.patial

---
+++ DBMVC/Controllers/PartialViewDemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CLModelLayer;
+using CLDataBaseLayer.DBOperations;
+
+namespace DBMVC.Controllers
+{
+    public class PartialViewDemoController : Controller
+    {
+        // GET: PartialViewDemo
+        EmployeeRepository repo = null;
+        public PartialViewDemoController()
+        {
+            repo = new EmployeeRepository();
+        }
+        public ActionResult Index()
+        {
+            var emplist = repo.GetAllEmployees();
+            return View(emplist);
+        }
+    }
+}
