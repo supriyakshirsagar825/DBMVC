d460dfe4e578353504e912af4d2225dbc62baf85
Supriya Kshirsagar <supriyakshirsagar9@gmail.com>
2021-03-30 15:28:05 -0400

    Multilpe submit button on view and file upload and download function in MVC5

--- DBMVC/Controllers/StudentController.cs
+++ DBMVC/Controllers/StudentController.cs
@@ -7,21 +7,68 @@ using CLModelLayer;
 using CLDataBaseLayer;
 using CLDataBaseLayer.DBOperations;
 using System.Threading.Tasks;
-
+using DBMVC.Models;
+using System.IO;
 namespace DBMVC.Controllers
 {
+    [AllowAnonymous]
     public class StudentController : Controller
     {
-        EmployeeRepository repo = null;
-       public StudentController()
-        {
-           repo= new EmployeeRepository();
-        }
+        
+      
         // GET: Student
         public ActionResult Index()
         {
            
             return View();
+        }
+        public ActionResult create()
+        {
+            ViewBag.city = city.getcitylist();
+            return View();
+        }
+        [HttpPost]
+        //public ActionResult create(student model,string create,string edit)
+        public ActionResult create(student model, string submit)
+        {
+            if(ModelState.IsValid)
+            {
+                string folderpath = Server.MapPath("~/App_Data/File");
+                string filename = Path.GetFileName(model.file.FileName);
+                string fullpath = Path.Combine(folderpath, filename);
+                model.file.SaveAs(fullpath);
+
+                return RedirectToAction("create");
+            }
+            ViewBag.city = city.getcitylist();
+            return View();
+        }
+        public FileResult Downloadfile()
+        {
+            //student s = new student();
+            string folderpath = Server.MapPath("~/App_Data/File");
+            string filename = Path.GetFileName("formvcdemo.txt");
+            string fullpath = Path.Combine(folderpath, filename);
+            return File(fullpath, "text/plain",filename);
+        }
+
+    }
+    public class city
+    {
+     public   int id { get; set; }
+      public  string text { get; set; }
+
+        public static List<city> getcitylist()
+        {
+            var res = new List<city>()
+            {
+                new city() { id = 1, text = "Mumbai" },
+                new city() { id = 2, text = "Pune" },
+                new city() { id = 3, text = "Nashik" },
+                new city() { id = 4, text = "Amravati" },
+                new city() { id = 5, text = "Nagpur" },
+            };
+            return res;
         }
     }
 }
---
+++ DBMVC/Models/student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DBMVC.Models
+{
+    public class student
+    {
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public int city { get; set; }
+
+        public HttpPostedFileBase file { get; set; }
+    }
+}
